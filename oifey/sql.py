a70ae1fcbede50b112b585dde84555cd85cda3ff
GiantFrog <35314154+GiantFrog@users.noreply.github.com>
2025-03-27 22:42:31 -0700

    Add methods for clean database interaction

--- oifey/sql.py
+++ oifey/sql.py
@@ -1,66 +1,113 @@
-import json
+from datetime import datetime, timedelta
 import os
 import psycopg
-
+from psycopg.types import TypeInfo
+from psycopg.types.hstore import register_hstore
 
 connection = psycopg.connect(os.getenv('DATABASE_CONNECTION'))
-cursor = connection.cursor()
+type_info = TypeInfo.fetch(connection, "hstore")
+register_hstore(type_info, connection)
+with connection.cursor() as cursor:
+    cursor.execute("""
+        create table if not exists users (
+            id bigint primary key not null,
+            sommie_pets int default 0,
+            favorite_hero varchar(64),
+            custom_aliases hstore default hstore(''),
+            banned_until timestamp,
+            ban_message text
+    );""")
+    connection.commit()
 
-banned_words = ['"', "'", "\\", "\n"]
+def sommie_pets():
+    with connection.cursor() as db:
+        db.execute("select sum(sommie_pets) from users;")
+        return db.fetchone()[0]
 
-class Table:
-    def __init__(self, key):
-        self.key = key
-        
-        # try pulling from table
-        # if not, create one
-        
+
+class User:
+    def __init__(self, user_id):
         try:
-            self.data = self.select()
-            
-        except psycopg.OperationalError:
-            cursor.execute(f"CREATE TABLE {self.key}(name, data)")
-            
-            self.data = self.select()
-            
-    def select(self):
-        "Gets all of the data in the table and stores in a dict for easy use"
-        current = cursor.execute(f"SELECT * FROM '{self.key}'")
-        
-        result = {}
-        
-        for value in current.fetchall():
-           result[value[0]] = json.loads(value[1])
+            self.id = int(user_id)
+            with connection.cursor() as db:
+                db.execute(f"select * from users where id = {self.id};")
+                database_row = db.fetchone()
+                if database_row is None:    # Never seen this person before.
+                    db.execute(f"insert into users(id) values ({self.id});")
+                    connection.commit()
+                    self.sommie_pets = 0
+                    self.favorite_hero = None
+                    self.custom_aliases = {}
+                    self.banned_until = None
+                    self.ban_message = None
+                else:   # Unwrap a tuple fetched from the DB into a nice python object!
+                    self.sommie_pets = database_row[1]
+                    self.favorite_hero = database_row[2]
+                    self.custom_aliases = database_row[3] or {}
+                    self.banned_until = database_row[4]
+                    self.ban_message = database_row[5]
+        except Exception as e:
+            print(f"ERROR: Couldn't make a User for ID {self.id}.\n{e}")
 
-        return result
-        
-    def get(self, name):
-        if not name in self.data:
-            return {}
-            
-        else:
-            return self.data[name]
-            
-    def update(self, name, value):
-        text = json.dumps(value)
-        
-        if not name in self.data:
-            cursor.execute(f"INSERT INTO '{self.key}' VALUES ({name}, '{text}');")
-        
-        else:
-            cursor.execute(f"UPDATE '{self.key}' SET data = '{text}' WHERE name = {name}")
-        
-        connection.commit()
+    # You may read from this class's variables all you want, but please only modify them using the below functions!
+    # Is one missing? Add it here! This way, we can keep the database synced and only updated in intentional ways.
+    def pet_sommie(self):
+        with connection.cursor() as db:
+            db.execute(f"update users set sommie_pets = sommie_pets + 1 where id = {self.id};")
+            connection.commit()
+        self.sommie_pets += 1
 
-        self.data[name] = value
-        
-    def drop(self, name):
-        if name in self.data: self.data.pop(name)
-        
-        cursor.execute(f"DELETE FROM '{self.key}' WHERE name = {name}")
-        
-        connection.commit()
-        
-# common used tables
-user = Table("user")
-server = Table("server")
+    def set_favorite_hero(self, hero: str):
+        if self.favorite_hero == hero:
+            return
+        with connection.cursor() as db:
+            db.execute(
+                f"update users set favorite_hero = (%s::text) where id = {self.id};",
+                [hero]                        # This silly little thing should fight SQL injection.
+            )                                 # I trust self.id because in init we make sure it's an int.
+            connection.commit()
+        self.favorite_hero = hero
+
+    def add_alias(self, alias: str, real_name: str):
+        if self.custom_aliases.get(alias) == real_name:
+            return
+        with connection.cursor() as db:
+            db.execute(
+                f"update users set custom_aliases = custom_aliases || (%s::hstore) where id = {self.id};",
+                [f'"{alias}"=>"{real_name}"']  # god it took forever to get the quotes and typing right
+            )
+            connection.commit()
+        self.custom_aliases[alias] = real_name
+
+    def remove_alias(self, alias: str):
+        if alias not in self.custom_aliases:
+            return
+        with connection.cursor() as db:
+            db.execute(
+                f"update users set custom_aliases = custom_aliases - (%s::text) where id = {self.id};",
+                [alias]
+            )
+            connection.commit()
+        del self.custom_aliases[alias]
+
+    def ban(self, duration=timedelta(weeks=2871), reason=None):  # default: 400 years
+        ban_timestamp = datetime.now() + duration
+        with connection.cursor() as db:
+            db.execute(
+                f"update users set banned_until = (%s::timestamp), ban_message = (%s::text) where id = {self.id};",
+                [ban_timestamp, reason]
+            )
+            connection.commit()
+        self.banned_until = ban_timestamp
+        self.ban_message = reason
+
+    def unban(self):
+        with connection.cursor() as db:
+            db.execute(f"update users set banned_until = null where id = {self.id};")
+            connection.commit()
+        self.banned_until = None
+
+    def is_banned(self) -> bool:
+        if self.banned_until is None:
+            return False
+        return self.banned_until > datetime.now()
