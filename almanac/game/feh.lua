11cfc4ccf1795a21890cfd1278af657597f18bbc
ToeThumbs <138536573+ToeThumbs@users.noreply.github.com>
2025-08-16 14:05:14 -0400

    add the extra dragonflowers

--- almanac/game/feh.lua
+++ almanac/game/feh.lua
@@ -658,29 +658,33 @@ function Character:flower_limit()
     local id = tonumber(self.data.id)
     
     local flower = 5
+    if id <= 1276 then
+        flower = flower +5
 
-    -- Heroes released after or during CYL7
-    if id <= 1130 then
-        flower = flower + 5
-    
-        -- Heroes released after or during CYL6
-        if id <= 985 then
+    -- Heroes released after or during CYL8
+        if id <= 1130 then
             flower = flower + 5
-        
-            if id <= 838 then
+    
+        -- Heroes released after or during CYL7
+            if id <= 985 then
                 flower = flower + 5
+        
+             -- Heroes released after or during CYL6
+                if id <= 838 then
+                    flower = flower + 5
                 
                 -- Heroes released after or during CYL5
-                if id <= 694 then
-                    flower = flower + 5
+                    if id <= 694 then
+                        flower = flower + 5
                     
                     -- Heroes released after or during CYL4
-                    if id <= 553 then
-                        flower = flower + 5
+                        if id <= 553 then
+                            flower = flower + 5
                         
                         -- Gen 1 Heroes that are also infantry
-                        if id <= 338 and self.data.move == "Infantry" then
-                            flower = flower + 5
+                            if id <= 338 and self.data.move == "Infantry" then
+                                flower = flower + 5
+                            end
                         end
                     end
                 end
